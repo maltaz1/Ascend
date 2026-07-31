16c784972b8fe4232fc1624c50f4bc9c610b8192
maltaz1 <1.75569467e+08+maltaz1@users.noreply.github.com>
2026-07-31 15:45:25 +0000

    chore: remove temp file

--- check_icons.cjs
+++
@@ -1,5 +0,0 @@
-const l = require('lucide-react');
-const icons = ['User', 'Palette', 'Bell', 'Cloud', 'Shield', 'Info', 'LogOut', 'Sparkles', 'Database', 'Lock', 'Save', 'Mail', 'CreditCard', 'XCircle', 'Download', 'Trash2', 'FileText', 'ExternalLink'];
-icons.forEach(icon => {
-  console.log(`${icon}: ${!!l[icon] ? 'OK' : 'MISSING'}`);
-});
